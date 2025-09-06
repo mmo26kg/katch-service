86443400fb8f852a3e129c63f9d424b641370be7
DucNguyen <hongducmmo@gmail.com>
2025-09-06 15:46:24 +0700

    Update Generate Module Script

---
+++ generate-module.js
@@ -0,0 +1,18 @@
+function generateModule(moduleName) {
+    const { execSync } = require('child_process');
+
+    // Generate module, controller, and service using Nest CLI
+    execSync(`nest generate module ${moduleName}/${moduleName} --no-spec`);
+    execSync(`nest generate controller ${moduleName} --no-spec`);
+    execSync(`nest generate service ${moduleName} --no-spec`);
+
+    console.log(`Module ${moduleName} generated successfully.`);
+}
+
+const moduleName = process.argv[2];
+if (!moduleName) {
+    console.error('Please provide a module name.');
+    process.exit(1);
+}
+
+generateModule(moduleName);
